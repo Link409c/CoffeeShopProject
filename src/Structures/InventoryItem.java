096a6f5190b2686426e4e2b660bd30fa03840b47
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-04-11 15:38:21 -0400

    updated class description

--- src/Structures/InventoryItem.java
+++ src/Structures/InventoryItem.java
@@ -1,7 +1,7 @@
 package Structures;
 
 /**
- * An object representing an inventory item tracked in a system.
+ * An object representing an inventory item tracked in a system. For example: Ingredients, Merchandise, etc.
  */
 public class InventoryItem extends Item{
 
